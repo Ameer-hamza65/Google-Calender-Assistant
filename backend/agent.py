0c949c22f5cf94dd2f308e80a25eaca1748f9658
Ameer-hamza65 <22-se-38@students.uettaxila.edu.pk>
2025-08-11 22:34:16 +0500

    bx

--- backend/agent.py
+++ backend/agent.py
@@ -14,7 +14,7 @@ llm = AzureChatOpenAI(
     max_tokens=None,
     timeout=None,  
     max_retries=2,  
-    api_key=os.getenv('AZURE_OPENAI_API') 
+    api_key=os.getenv('AZURE_OPENAI_API')  
 )  
     
 graph = create_react_agent(llm, tools, checkpointer=memory)
