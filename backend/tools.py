f3c94baf49c8c19866b3d4abbe7acbace0ea9f37
Ameer-hamza65 <22-se-38@students.uettaxila.edu.pk>
2025-08-11 20:52:24 +0500

    cv

--- backend/tools.py
+++ backend/tools.py
@@ -8,7 +8,7 @@ from langchain_core.tools import tool
 
 #Creating tools from the Langchain Google Calender kit 
 tool1=CalendarDeleteEvent()
-tool2=CalendarCreateEvent()  
+tool2=CalendarCreateEvent()   
 tool3=CalendarSearchEvents() 
 tool4=GetCalendarsInfo()  
 tool5=GetCurrentDatetime()
